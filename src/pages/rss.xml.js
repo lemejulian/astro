ce8813a835d94a9cf23d78e6c7485e18d4e45cc4
julian <lemejulian@gmail.com>
2025-01-12 20:36:51 -0300

    final

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  return rss({
+    title: 'Alumno de Astro | Blog',
+    description: 'Mi viaje de aprendizaje de Astro',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
 }
